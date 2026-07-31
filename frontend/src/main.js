ea7947558df25851a2534381f849e19334547156
UniGood <UniGood@163.com>
2026-07-31 19:08:11 +0800

    feat: 集成 vue-i18n 到 main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,6 +4,7 @@ import hljs from 'highlight.js/lib/core'
 import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia'
+import i18n from './i18n'
 
 // 配置 hljs 给 naive-ui 的 n-code 组件使用
 const app = createApp(App)
@@ -13,6 +14,7 @@ const pinia = createPinia()
 
 app.use(pinia)
 app.use(router)
+app.use(i18n)
 app.use(naive)
 
 app.mount('#app')
